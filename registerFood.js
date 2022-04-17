340205be54d8a6b00e9872d2b8842e9ead6b3653
Ako-Tako <akoide18@gmail.com>
2022-04-16 19:26:52 -0700

    register page upload

---
+++ registerFood.js
@@ -0,0 +1,19 @@
+function clickRegister() {
+    // would transfer data to database
+
+    // clear page and thank for register data
+
+    // get ID'd elements with hide feature and set display property to none
+    document.getElementById("hide1").style.display = "none";
+    document.getElementById("hide2").style.display = "none";
+    document.getElementById("hide3").style.display = "none";
+    document.getElementById("hide4").style.display = "none";
+    document.getElementById("hide5").style.display = "none";
+    document.getElementById("hide6").style.display = "none";
+    document.getElementById("hide7").style.display = "none";
+
+    // show "you registered!" text that has been hiding there all along >:3
+    document.getElementById("toShow").style.display = "inline-block";
+
+
+}
